10eabbd591b5dcbf6b681f23b89102cfefd033a6
shimniok@gmail.com <shimniok@gmail.com>
2018-07-12 16:45:53 -0600

    added timer to call handler at regular interval, fixed ctrl-c quit and ^D (EOF) quit

--- rock.py
+++ rock.py
@@ -4,23 +4,45 @@ import rockBlock
 from rockBlock import rockBlockProtocol
 import signal
 import sys
+import threading
 
 class RockClient(rockBlockProtocol):
 
     def main(self):
         self.rb = rockBlock.rockBlock("/dev/ttyUSB0", self)
         signal.signal(signal.SIGINT, self.signal_handler)
+        self.timer_start()
 
         while True:
-            text = raw_input("Message?> ").strip()
-            if text != "":
-                print("Sending: <{}>".format(text))
-                self.rb.sendMessage(text.strip())
+            try:
+                text = raw_input("Message?> ").strip()
+                if text != "":
+                    self.timer.cancel()
+                    print("Sending: <{}>".format(text))
+                    self.rb.sendMessage(text.strip())
+                    self.timer.start()
+            except EOFError:
+                self.quit()
 
-    def signal_handler(sig, frame):
+    def quit(self):
+        self.timer_stop()
         print('\nExiting.')
         self.rb.close()
         sys.exit(0)
+
+    def timer_start(self):
+        self.timer = threading.Timer(3.0, self.timer_handler)
+        self.timer.start()
+
+    def timer_stop(self):
+        self.timer.cancel()
+
+    def timer_handler(self):
+        print("\nTimer handler <<-- insert mtrecv here")
+        self.timer_start()
+
+    def signal_handler(self, sig, frame):
+        self.quit()
 
     def rockBlockTxStarted(self):
         print "rockBlockTxStarted"
