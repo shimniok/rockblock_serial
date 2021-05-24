c4d92edc166d6278415fbd9ed6cbe3780037c78d
Michael Shimniok <shimniok@gmail.com>
2021-05-24 12:22:04 -0600

    reformatting

--- rockblock.py
+++ rockblock.py
@@ -18,10 +18,12 @@ import sys
 import time
 import serial
 
+
 class RockBlockEvent(object):
     def __init__(self, value, status):
         self.value = value
         self.status = status
+
 
 class RockBlockProtocol(object):
 
@@ -29,38 +31,39 @@ class RockBlockProtocol(object):
     STATUS_SUCCESS = 1
     STATUS_ERROR = 2
 
-    #RAW OUTPUT
-    def process_serial(self, text):pass
+    # RAW OUTPUT
+    def process_serial(self, text): pass
 
-    #STATUS
-    def status(self, message, status):pass
+    # STATUS
+    def status(self, message, status): pass
 
-    #CONNECTION
-    def rockBlockConnected(self):pass
-    def rockBlockDisconnected(self):pass
+    # CONNECTION
+    def rockBlockConnected(self): pass
+    def rockBlockDisconnected(self): pass
 
-    #NETWORK TIME
-    def rockBlockNetworkTime(self, event):pass
+    # NETWORK TIME
+    def rockBlockNetworkTime(self, event): pass
 
-    #IMEI
-    def imei_event(self, event):pass
+    # IMEI
+    def imei_event(self, event): pass
 
-    #SIGNAL
-    def signal_event(self, event):pass
+    # SIGNAL
+    def signal_event(self, event): pass
 
-    #SESSION
-    def rockBlockSession(self, mo_status, mo_msn, mt_status, mt_msn, mt_length, mt_queued):pass
-        
-    #MT
-    def rockBlockRxStarted(self):pass
-    def rockBlockRxFailed(self):pass
-    def rockBlockRxReceived(self, mtmsn, data):pass
-    def rockBlockRxMessageQueue(self, count):pass
+    # SESSION
+    def rockBlockSession(self, mo_status, mo_msn, mt_status,
+                         mt_msn, mt_length, mt_queued): pass
 
-    #MO
-    def rockBlockTxStarted(self):pass
-    def rockBlockTxFailed(self):pass
-    def rockBlockTxSuccess(self, momsn):pass
+    # MT
+    def rockBlockRxStarted(self): pass
+    def rockBlockRxFailed(self): pass
+    def rockBlockRxReceived(self, mtmsn, data): pass
+    def rockBlockRxMessageQueue(self, count): pass
+
+    # MO
+    def rockBlockTxStarted(self): pass
+    def rockBlockTxFailed(self): pass
+    def rockBlockTxSuccess(self, momsn): pass
 
 
 class RockBlockException(Exception):
@@ -85,23 +88,28 @@ class RockBlockPortException(RockBlockException):
 
 
 class RockBlock(object):
-    IRIDIUM_EPOCH = 1399818235000   
-    #May 11, 2014, at 14:23:55 (This will be 're-epoched' every couple of years!)
-    
+    IRIDIUM_EPOCH = 1399818235000
+    # May 11, 2014, at 14:23:55 (This will be 're-epoched' every couple of
+    # years!)
+
     def __init__(self, portId, callback):
         self.s = None
-        #print("port={}".format(portId))
+        # print("port={}".format(portId))
         self.portId = portId
         self.callback = callback
-        self.autoSession = True     #When True, we'll automatically initiate additional sessions if more messages to download
+        # When True, we'll automatically initiate additional sessions if more
+        # messages to download
+        self.autoSession = True
         self.SIGNAL_THRESHOLD = 2
 
         # try:
         self.s = serial.Serial(self.portId, 19200, timeout=5)
-        if not (self._disableEcho() and self._disableFlowControl and self._disableRingAlerts()):
+        if not (self._disableEcho()
+                and self._disableFlowControl
+                and self._disableRingAlerts()):
             self.close()
             raise RockBlockPortException
-            
+
     ##
     # Serial Specific
     ##
@@ -142,7 +150,7 @@ class RockBlock(object):
     ##
     # Parsing
     ##
-    
+
     def send_command(self, command):
         self.s.write(command.encode('utf-8') + b'\r')
         self.callback.process_serial(command)
@@ -162,17 +170,20 @@ class RockBlock(object):
     ##
 
     def messageCheck(self):
-        self.callback.status("Message RX started...", RockBlockProtocol.STATUS_INFO)
+        self.callback.status("Message RX started...",
+                             RockBlockProtocol.STATUS_INFO)
         if self.connectionOk():
             self._perform_session()
 
     def sendMessage(self, msg):
-        self.callback.status("Message TX started...", RockBlockProtocol.STATUS_INFO)
+        self.callback.status("Message TX started...",
+                             RockBlockProtocol.STATUS_INFO)
         if not self.connectionOk():
             return False
 
         if not self._queueMessage(msg):
-            self.callback.status("queue message failed", RockBlockProtocol.STATUS_ERROR)
+            self.callback.status("queue message failed",
+                                 RockBlockProtocol.STATUS_ERROR)
             return False
 
         return self._perform_session()
@@ -180,12 +191,13 @@ class RockBlock(object):
     def connectionOk(self):
         self._verify_serial_connected()
 
-        #Check valid Network Time
+        # Check valid Network Time
         if not self._isNetworkTimeValid():
-            self.callback.status("Invalid network time", RockBlockProtocol.STATUS_ERROR)
+            self.callback.status("Invalid network time",
+                                 RockBlockProtocol.STATUS_ERROR)
             return False
 
-        #Check signal strength
+        # Check signal strength
         signal = self.requestSignalStrength()
         if not signal > 0:
             self.callback.status("no signal", RockBlockProtocol.STATUS_ERROR)
@@ -207,19 +219,19 @@ class RockBlock(object):
     # this command has been issued...
     def setup(self):
         self._verify_serial_connected()
-        #Disable Flow Control
+        # Disable Flow Control
         self.send_command("AT&K0")
         self.expect("OK")
-        
-        #Store Configuration into Profile0
+
+        # Store Configuration into Profile0
         self.send_command("AT&W0")
         self.expect("OK")
 
-        #Use Profile0 as default
+        # Use Profile0 as default
         self.send_command("AT&Y0")
         self.expect("OK")
 
-        #Flush Memory
+        # Flush Memory
         self.send_command("AT*F")
         self.expect("OK")
         return True
@@ -232,7 +244,7 @@ class RockBlock(object):
         self._verify_serial_connected()
         self.send_command("AT")
         response = self.expect("OK")
-        
+
         return response != None
 
     # TODO: update signal status from here
@@ -246,7 +258,8 @@ class RockBlock(object):
             signal = int(response)
         except:
             signal = 0
-        self.callback.status("Signal: {:d}".format(signal), RockBlockProtocol.STATUS_INFO)
+        self.callback.status("Signal: {:d}".format(
+            signal), RockBlockProtocol.STATUS_INFO)
 #        ev = RockBlockEvent(signal, signal > 0)
 #        self.callback.signal_event(ev)
         return signal
@@ -255,14 +268,17 @@ class RockBlock(object):
         self._verify_serial_connected()
         self.send_command("AT-MSSTM")
         response = self.expect("-MSSTM: ")
-        self.expect("OK")   #OK
+        self.expect("OK")  # OK
         utc = 0
         if response == None:
-            self.callback.status("no network time response", RockBlockProtocol.STATUS_ERROR)
+            self.callback.status("no network time response",
+                                 RockBlockProtocol.STATUS_ERROR)
         elif "no network service" in response:
-            self.callback.status("no network service", RockBlockProtocol.STATUS_ERROR)
+            self.callback.status("no network service",
+                                 RockBlockProtocol.STATUS_ERROR)
         else:
-            self.callback.status("network time success", RockBlockProtocol.STATUS_SUCCESS)
+            self.callback.status("network time success",
+                                 RockBlockProtocol.STATUS_SUCCESS)
             utc = int(response, 16)
             utc = int((self.IRIDIUM_EPOCH + (utc * 90))/1000)
             self.callback.rockBlockNetworkTime(RockBlockEvent(utc, True))
@@ -281,9 +297,12 @@ class RockBlock(object):
         self._verify_serial_connected()
 
         if len(msg) > 340:
-            self.callback.status("message must be 340 bytes or less", RockBlockProtocol.STATUS_ERROR)
+            self.callback.status(
+                "message must be 340 bytes or less", RockBlockProtocol.STATUS_ERROR)
             return False
 
+        self.callback.status("adding message to buffer",
+                             RockBlockProtocol.STATUS_INFO)
         self.callback.status("adding message to buffer", RockBlockProtocol.STATUS_INFO)
 
         command = "AT+SBDWB={:d}".format(len(msg))
@@ -302,7 +321,8 @@ class RockBlock(object):
             self.callback.status("message queued", RockBlockProtocol.STATUS_SUCCESS)
             return True
         else:
-            self.callback.status("READY message not found", RockBlockProtocol.STATUS_ERROR)
+            self.callback.status("READY message not found",
+                                 RockBlockProtocol.STATUS_ERROR)
             return False
 
     def _perform_session(self):
@@ -316,7 +336,8 @@ class RockBlock(object):
         self.expect("OK")
 
         if response == None:
-            self.callback.status("invalid session response", RockBlockProtocol.STATUS_ERROR)
+            self.callback.status("invalid session response",
+                                 RockBlockProtocol.STATUS_ERROR)
             return False
 
         parts = response.split(",")
@@ -327,31 +348,35 @@ class RockBlock(object):
         mtLength = int(parts[4])
         mtQueued = int(parts[5])
 
-        self.callback.rockBlockSession(moStatus, moMsn, mtStatus, mtMsn, mtLength, mtQueued)
+        self.callback.rockBlockSession(
+            moStatus, moMsn, mtStatus, mtMsn, mtLength, mtQueued)
 
-        #Mobile Originated
+        # Mobile Originated
         if moStatus <= 4:
             self._clearMoBuffer()
 
-        if mtStatus == 1 and mtLength > 0: 
+        if mtStatus == 1 and mtLength > 0:
             # SBD message successfully received from the GSS.
             msg = self._read_mt_message()
             self.callback.rockBlockRxReceived(mtMsn, msg)
         # TODO: handle mtStatus error values
 
         if moStatus <= 4:
-            self.callback.status("session succeeded", RockBlockProtocol.STATUS_SUCCESS)
+            self.callback.status("session succeeded",
+                                 RockBlockProtocol.STATUS_SUCCESS)
             return True
         else:
-            self.callback.status("session failed", RockBlockProtocol.STATUS_ERROR)
+            self.callback.status(
+                "session failed", RockBlockProtocol.STATUS_ERROR)
             return False
 
     def _read_mt_message(self):
         self._verify_serial_connected()
 
-        self.callback.status("Reading MT message", RockBlockProtocol.STATUS_INFO)
+        self.callback.status("Reading MT message",
+                             RockBlockProtocol.STATUS_INFO)
 
-        self.send_command("AT+SBDRB") 
+        self.send_command("AT+SBDRB")
         length = int.from_bytes(self.s.read(2), byteorder='big')
         msg = ""
         if length > 0:
@@ -369,20 +394,23 @@ class RockBlock(object):
         if length > 0:
             # compare checksum
             if mysum != cksum:
-                self.callback.status("checksum mismatch {:d} {:d}".format(mysum, cksum), RockBlockProtocol.STATUS_ERROR)
+                self.callback.status("checksum mismatch {:d} {:d}".format(
+                    mysum, cksum), RockBlockProtocol.STATUS_ERROR)
             return msg.decode('utf-8')
-        
+
         return None
 
     def _clearMoBuffer(self):
         self._verify_serial_connected()
-        self.callback.status("clearing MO buffer", RockBlockProtocol.STATUS_INFO)
+        self.callback.status("clearing MO buffer",
+                             RockBlockProtocol.STATUS_INFO)
         self.send_command("AT+SBDD0")
         r1 = self.expect("0")
-        self.serial_readline()  #BLANK
-        self.serial_readline()  #OK
+        self.serial_readline()  # BLANK
+        self.serial_readline()  # OK
         if r1 == None:
-            self.callback.status("clear buffer: {} expected 0".format(r1), RockBlockProtocol.STATUS_ERROR)
+            self.callback.status("clear buffer: {} expected 0".format(
+                r1), RockBlockProtocol.STATUS_ERROR)
             return False
         else:
             return True
