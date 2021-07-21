880431e2c9aa7f410397c006c2d971800b772dc4
Michael Shimniok <shimniok@gmail.com>
2021-07-21 10:50:31 -0600

    implement reconnect to rabbitmq

--- app/amqp.service.js
+++ app/amqp.service.js
@@ -1,14 +1,38 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib/callback_api');
+var amqp = require("amqplib/callback_api");
+
+var me = "amqp service";
 
 module.exports = {
+  reconnect: (url, callback) => {
+    new Promise((resolve) => {
+      setTimeout(resolve, 10000);
+    }).then(() => {
+      module.exports.connect(url, callback);
+    });
+  },
+
   connect: (url, callback) => {
+    console.log("%s: connecting", me);
     amqp.connect(url, function (err, connection) {
       if (err) {
-        console.log('connect error:', err);
+        console.log("%s: connect error:", me, err);
+        module.exports.reconnect(url, callback);
       } else {
+        console.log("%s: connected", me);
         connection.createChannel((err, channel) => {
+          console.log("%s: channel created", me);
+
+          channel.on("close", () => {
+            console.log("%s: channel closed", me);
+            module.exports.reconnect(url, callback);
+          });
+
+          channel.on("error", (err) => {
+            console.log("%s: channel error: %s", err);
+          });
+
           callback(channel);
         });
       }
@@ -17,13 +41,18 @@ module.exports = {
 
   receive: (channel, queue, durable, recv) => {
     channel.assertQueue(queue, { durable: durable });
-    console.log(' [*] Waiting for messages in %s.', queue);
+    console.log("%s: [*] Waiting for messages in %s.", me, queue);
     channel.consume(
       queue,
       function (msg) {
-        recv(msg.fields.routingKey, msg.content.toString('utf-8'));
+        recv(msg.fields.routingKey, msg.content.toString("utf-8"));
       },
       { noAck: true }
     );
   },
+
+  send: (channel, message) => {
+    if (channel)
+      channel.sendToQueue("outbox", Buffer.from(message));
+  }
 };
--- app/server.js
+++ app/server.js
@@ -1,57 +1,62 @@
-const app = require('express')();
-const http = require('http').Server(app);
+const app = require("express")();
+const http = require("http").Server(app);
 // const httpProxy = require('http-proxy');
-const io = require('socket.io')(http, {
+const io = require("socket.io")(http, {
   cors: {
-    origin: 'http://localhost:4200/',
-    methods: ['GET', 'POST'],
+    origin: "http://localhost:4200/",
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
-const path = require('path');
-const amqp = require('./amqp.service.js');
+const path = require("path");
+const amqpService = require("./amqp.service.js");
 
-var amqpUrl = 'amqp://localhost/';
+var amqpUrl = "amqp://localhost/";
+
+var me = "app server";
 
 // Temporary Data Store
 var messages = [];
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'angular-build', 'index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "angular-build", "index.html"));
 });
 
 // AMQP
-amqp.connect(amqpUrl, function (channel) {
-  // SocketIO Events
-  io.on('connection', (socket) => {
-    amqp.receive(channel, 'signal', false, (key, data) => {
-      console.log('server.ts: signal recv key =', key, 'data =', data);
-      io.emit('signal', data);
+amqpService.connect(amqpUrl, (channel) => {
+
+  io.on("connection", (socket) => {
+    console.log("%s: socket %s has connected", me, socket.id);
+
+    amqpService.receive(channel, "signal", false, (key, data) => {
+      console.log("%s: signal recv key=%s data=%s", me, key, data);
+      io.emit("signal", data);
     });
 
-    amqp.receive(channel, 'inbox', true, (key, data) => {
-      console.log('server.ts: messages recv key =', key, 'data=', data);
+    amqpService.receive(channel, "inbox", true, (key, data) => {
+      console.log("%s: signal recv key=%s data=%s", me, key, data);
       messages.push(data);
-      io.emit('messages', messages);
+      io.emit("messages", messages);
     });
 
-    socket.on('send', (message) => {
-      console.log('server.ts: mo send');
-      channel.sendToQueue('outbox', Buffer.from(message));
+    socket.on("send", (message) => {
+      console.log("%s: mo send msg=%s", me, message);
+      amqpService.send(channel, message);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("%s: socket disconnected - o nooo!", me);
+    });
+
+    socket.on("error", () => {
+      console.log("%s: socket error - o nooo!", me);
     });
 
     // send current list of messages
-    io.emit('messages', messages);
-
-    // TODO: handle connection failure / disconnect
-    socket.on('disconnect', function () {
-      socket.emit('Socket disconnected - o nooo!');
-    });
-
-    console.log(`Socket ${socket.id} has connected`);
+    io.emit("messages", messages);
   });
 });
 
 var listener = http.listen(4444, () => {
-  console.log('Listening on port ' + listener.address().port);
+  console.log("%s: listening on port %d", me, listener.address().port);
 });
